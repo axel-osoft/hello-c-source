3bfd1231babc1c2b27ad5742f42169011cda480b
axel-osoft <75009084+axel-osoft@users.noreply.github.com>
2024-04-04 13:51:58 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Hello, Brave New World!");
+   printf("Hello, Super Brave New World!");
    return 0;
 }
